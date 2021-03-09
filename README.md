9a07db899dfa6fe8654d5a634f3ed68beef326c5
daniel-kanchev <daniel.kanchev@adata.pro>
2021-03-09 11:49:53 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,12 @@
+URL: https://www.rothschildandco.com/fr/actualites/
+
+    Spider name: rothschildandco
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
+- gets articles from current year
---
+++ rothschildandco/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'rothschildandco'
+SPIDER_MODULES = ['rothschildandco.spiders']
+NEWSPIDER_MODULE = 'rothschildandco.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'rothschildandco.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
